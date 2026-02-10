3951163d6256c86a88123638480c63ca36262c86
Konstantin Pivnov <vaproloff@ya.ru>
2026-02-10 22:55:38 +0800

    add db connection and user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
+from app.models import user
+from app.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
 
 @app.get("/health")
 def health():
---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
