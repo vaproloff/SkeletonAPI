269d52aa6406ec3fca30ffaa973ff4cffa94a298
Konstantin Pivnov <vaproloff@ya.ru>
2026-02-11 23:34:53 +0800

    add user registration schemas and endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 
-from app.models import user
-from app.database import engine, Base
+from app.core.security import hash_password
+from app.models.user import User
+from app.database import engine, Base, get_db
+from app.schemas.user import UserOut, UserCreate
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,3 +15,22 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/register", response_model=UserOut)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        email=user.email,
+        hashed_password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
