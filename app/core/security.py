b7c78be5f181f604596fb2358445f0a15ff3f73e
Konstantin Pivnov <vaproloff@ya.ru>
2026-02-12 22:34:16 +0800

    add login endpoint and jwt token

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,20 @@
+from jose import jwt
 from passlib.context import CryptContext
+
+ALGORITHM = "HS256"
+SECRET_KEY = 'super-secret-key'
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict) -> str:
+    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
-from app.core.security import hash_password
+from app.core.security import hash_password, verify_password, create_access_token
 from app.models.user import User
 from app.database import engine, Base, get_db
 from app.schemas.user import UserOut, UserCreate
@@ -34,3 +34,16 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
 
     return new_user
 
+
+@app.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    existing_user: User = db.query(User).filter(User.email == email).first()
+    if not existing_user or not verify_password(password, existing_user.hashed_password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    access_token = create_access_token({"email": email})
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
