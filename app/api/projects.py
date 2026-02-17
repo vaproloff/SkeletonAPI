744ca82bd7ae930e22fd4ba0904a2893d39b8713
Konstantin Pivnov <vaproloff@ya.ru>
2026-02-17 20:52:26 +0800

    add getting preject by id

--- app/api/projects.py
+++ app/api/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.core.auth import get_current_user
@@ -32,3 +32,18 @@ def get_projects(
 ):
     projects = db.query(Project).filter(Project.owner_id == current_user.id).all()
     return projects
+
+
+@router.get("/{project_id}", response_model=ProjectOut)
+def get_project(
+        project_id: int,
+        current_user: User = Depends(get_current_user),
+        db: Session = Depends(get_db)
+):
+    project = (db.query(Project)
+               .filter(Project.id == project_id, Project.owner_id == current_user.id)
+               .first())
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+
+    return project
